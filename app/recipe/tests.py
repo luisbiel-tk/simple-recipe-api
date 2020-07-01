9ff8433085f2c00f5512f84f336e4f1e7a20e47d
luisbiel <luis.biel@travelperk.com>
2020-07-02 01:25:26 +0200

    Add enpoint to get all recipes

--- app/recipe/tests.py
+++ app/recipe/tests.py
@@ -1,3 +1,30 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+RECIPE_URL = reverse('recipe:recipe-list')
+
+
+class RecipeApiTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_all_recipes(self):
+
+        Recipe.objects.create(name='Tortilla', description='Tortilla de patatas con cebolla')
+        Recipe.objects.create(name='Banderilla', description='Pincho de olivas y boquerones')
+
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+
+        res = self.client.get(RECIPE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+        self.assertEqual(len(res.data), len(serializer.data))
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+class RecipeViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
