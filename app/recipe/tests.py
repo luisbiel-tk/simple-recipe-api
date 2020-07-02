eb76ab5180979809f40522542c33731e763e8bfc
luisbiel <luis.biel@travelperk.com>
2020-07-02 11:34:59 +0200

    Add recipe detail endpoint

--- app/recipe/tests.py
+++ app/recipe/tests.py
@@ -9,6 +9,9 @@ from .serializers import RecipeSerializer
 
 RECIPE_URL = reverse('recipe:recipe-list')
 
+def recipe_detail_url(recipe_id):
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
 
 class RecipeApiTest(TestCase):
 
@@ -16,7 +19,6 @@ class RecipeApiTest(TestCase):
         self.client = APIClient()
 
     def test_get_all_recipes(self):
-
         Recipe.objects.create(name='Tortilla', description='Tortilla de patatas con cebolla')
         Recipe.objects.create(name='Banderilla', description='Pincho de olivas y boquerones')
 
@@ -28,3 +30,15 @@ class RecipeApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
         self.assertEqual(len(res.data), len(serializer.data))
+
+    def test_get_recipe_detail(self):
+        recipe = Recipe.objects.create(name='Caracoles', description='Caracoles a la llauna o con sofrito')
+        res = self.client.get(recipe_detail_url(recipe.id))
+        serialized_recipe = RecipeSerializer(recipe)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serialized_recipe.data)
+
+    def test_get_recipe_detail_returns_not_found(self):
+        res = self.client.get(recipe_detail_url(1234))
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, mixins
 from .models import Recipe
 from .serializers import RecipeSerializer
 
-class RecipeViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class RecipeViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
 
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
